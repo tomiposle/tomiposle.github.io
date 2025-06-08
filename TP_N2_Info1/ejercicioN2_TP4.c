f351256ea76bcd502e2ee21cacc99612072a3ce7
Tomas Posleman <159287745+tomiposle@users.noreply.github.com>
2025-06-08 19:00:11 -0300

    Update ejercicioN2_TP4.c

--- TP_N2_Info1/ejercicioN2_TP4.c
+++ TP_N2_Info1/ejercicioN2_TP4.c
@@ -4,20 +4,16 @@
 int main() {
     float peso, altura, bmi;
 
-    // Solicitar datos al usuario
     printf("Ingrese el peso en kg: ");
     scanf("%f", &peso);
 
     printf("Ingrese la altura en metros: ");
     scanf("%f", &altura);
 
-    // Calcular el índice de masa corporal
     bmi = peso / (altura * altura);
 
-    // Mostrar el resultado del BMI
     printf("\nSu índice de masa corporal es: %.2f\n\n", bmi);
 
-    // Mostrar la tabla de condiciones
     printf("Índice\t\tCondición\n");
     printf("-----------------------------\n");
     printf("<18.5\t\tBajo peso\n");
@@ -25,7 +21,7 @@ int main() {
     printf("25.0 a 29.9\tSobrepeso\n");
     printf(">=30\t\tObesidad\n");
 
-    // Determinar la condición del usuario
+    // Determinar la condición del usuario (nuevo agregado al script del TP numero 3)
     printf("\nUsted se encuentra en la categoría: ");
 
     if (bmi < 18.5) {
