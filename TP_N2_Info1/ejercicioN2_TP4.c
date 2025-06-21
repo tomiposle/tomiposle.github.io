10d118c72f0b082408f5fed515905d58821237f5
Tomas Posleman <159287745+tomiposle@users.noreply.github.com>
2025-06-21 20:36:56 -0300

    Update ejercicioN2_TP4.c con agregados de TP5

--- TP_N2_Info1/ejercicioN2_TP4.c
+++ TP_N2_Info1/ejercicioN2_TP4.c
@@ -4,16 +4,30 @@
 int main() {
     float peso, altura, bmi;
 
-    printf("Ingrese el peso en kg: ");
-    scanf("%f", &peso);
+    // Validación del peso (debe ser mayor que 0)
+    do {
+        printf("Ingrese el peso en kg: ");
+        scanf("%f", &peso);
+        if (peso <= 0) {
+            printf("Error: El peso debe ser un número positivo mayor que cero.\n");
+        }
+    } while (peso <= 0);
 
-    printf("Ingrese la altura en metros: ");
-    scanf("%f", &altura);
+    // Validación de la altura (debe ser mayor que 0, agregado al script del TP numero 5)
+    do {
+        printf("Ingrese la altura en metros: ");
+        scanf("%f", &altura);
+        if (altura <= 0) {
+            printf("Error: La altura debe ser un número positivo mayor que cero.\n");
+        }
+    } while (altura <= 0);
 
+    // Cálculo del índice de masa corporal
     bmi = peso / (altura * altura);
 
     printf("\nSu índice de masa corporal es: %.2f\n\n", bmi);
 
+    // Tabla de referencia
     printf("Índice\t\tCondición\n");
     printf("-----------------------------\n");
     printf("<18.5\t\tBajo peso\n");
@@ -21,7 +35,7 @@ int main() {
     printf("25.0 a 29.9\tSobrepeso\n");
     printf(">=30\t\tObesidad\n");
 
-    // Determinar la condición del usuario (nuevo agregado al script del TP numero 3)
+    // Clasificación del usuario (agregado al script del TP numero 3)
     printf("\nUsted se encuentra en la categoría: ");
 
     if (bmi < 18.5) {
@@ -36,3 +50,4 @@ int main() {
 
     return 0;
 }
+
